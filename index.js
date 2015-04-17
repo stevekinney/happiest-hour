716df7ec19fcff55c50d9e22cd5a0bd7d150b8cf
Steve Kinney <hello@stevekinney.net>
2015-04-17 16:06:29 -0600

    Set up a basic WebSocket connection

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('./public/index.html');
+  res.sendfile('./public/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  var x = 1;
+  setInterval(function () {
+    console.log('Send shit:', ++x);
+    socket.emit('message', x);
+  }, 1000);
 });
 
 http.listen(3000, function(){
